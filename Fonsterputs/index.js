824afb309d9821773ddce33de2e9b068017c581b
AlvinOls <alvols238@edu.linkoping.se>
2026-05-06 11:07:26 +0000

    Klar med nodemailer

--- Fonsterputs/index.js
+++ Fonsterputs/index.js
@@ -1,13 +1,24 @@
-
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const mongoose = require("mongoose");
+const nodemailer = require("nodemailer");
 const port = 5001;
+
 const Booking = require("./models/Booking");
 
 const dbURI =
   "mongodb+srv://alvols238_db_user:AlvinAlvarGA@fonsterputs.nqcsayp.mongodb.net/fonsterputs";
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
 mongoose
   .connect(dbURI)
@@ -18,11 +29,9 @@ app.set("view engine", "pug");
 app.set("views", "./views");
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
-
-app.use(express.urlencoded({ extended: true }));
 
 app.listen(port, () => {
   console.log("hej");
@@ -35,13 +44,14 @@ app.get("/", (req, res) => {
 app.get("/tack", (req, res) => {
   res.render("tack");
 });
-app.post("/form", async (req, res) => {
-  console.log(req.body);
-  res.redirect("/tack");
-});
 
 app.get("/omoss", (req, res) => {
   res.render("omoss");
+});
+
+app.post("/form", async (req, res) => {
+  console.log(req.body);
+  res.redirect("/tack");
 });
 
 app.post("/tack", (req, res) => {
@@ -54,13 +64,22 @@ app.post("/boka", async (req, res) => {
     await Booking.create({
       name: req.body.name,
       email: req.body.email,
-        adress: req.body.address,
-        propertyType: req.body.propertyType,
-        meddelande: req.body.meddelande
+      adress: req.body.address,
+      propertyType: req.body.propertyType,
+      meddelande: req.body.meddelande,
     });
-    
+
+    const mailOptions = {
+      from: "ungfonsterputs@gmail.com", // Ändra till din e-postadress
+      to: "ungfonsterputs@gmail.com", // Skicka till den e-postadress som användaren angav
+      subject: `Ny putsning bokad av ${req.body.name}`,
+      text: `Namn: ${req.body.name}\nE-post: ${req.body.email}\nAdress: ${req.body.address}\nTyp av bostad: ${req.body.propertyType}\nMeddelande: ${req.body.meddelande}`,
+    };
+
+    await transporter.sendMail(mailOptions);
     res.redirect("/tack");
   } catch (err) {
+    console.error("E-post eller bokning misslyckades:", err);
     res.status(500).send("Ett fel uppstod: " + err.message);
   }
 });
